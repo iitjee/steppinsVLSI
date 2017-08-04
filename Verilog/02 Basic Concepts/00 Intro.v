9f7c8842a230bd3bb63f6b388025400ee5c85241
iitjee <iit.jee.help.me@gmail.com>
2017-08-04 10:35:15 +0530

    Create 00 Intro.v

---
+++ Verilog/02 Basic Concepts/00 Intro.v
@@ -0,0 +1,2 @@
+Data types in Verilog model actual data storage and switch elements in hardware very closely. This chapter may seem dry, but 
+understanding these concepts is a necessary foundation for the successive chapters.
